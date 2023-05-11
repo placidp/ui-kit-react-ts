038513808c0fd1f0e94fbcd404f3105bb264af40
gepiko <aoriaira@gmail.com>
2023-05-11 20:36:36 +0300

    scaffolding

---
+++ src/components/App/App.tsx
@@ -0,0 +1,11 @@
+import { Layout } from 'components'
+
+export const App: React.FC = () => {
+	return (
+		<div className='App'>
+			<Layout>
+				<h2>UI KIT</h2>
+			</Layout>
+		</div>
+	)
+}
---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,10 @@
+interface ILayoutProps {
+	children?: React.ReactNode
+}
+export const Layout: React.FC<ILayoutProps> = ({ children }) => {
+	return (
+		<div className='Layout'>
+			<div className='Layout-Container'>{children}</div>
+		</div>
+	)
+}
